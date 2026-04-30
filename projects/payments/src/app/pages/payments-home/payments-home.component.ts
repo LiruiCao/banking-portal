124a9d9fa166ff4e524edc627056f6748d3fa467
LiruiCao <47792631+LiruiCao@users.noreply.github.com>
2026-04-29 22:00:34 -0400

    link Payments home Interac e-Transfer card to flow

--- projects/payments/src/app/pages/payments-home/payments-home.component.ts
+++ projects/payments/src/app/pages/payments-home/payments-home.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-payments-home',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './payments-home.component.html',
   styleUrl: './payments-home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
